c18cf21f623c379ed93320bc463fcd4e2d679170
diphen102 <duyphanqwer12@gmail.com>
2026-09-21 11:25:26 +0700

    small update

--- frontend/src/components/MusicPlayer.jsx
+++ frontend/src/components/MusicPlayer.jsx
@@ -61,7 +61,7 @@ export default function MusicPlayer({ autoPlayTrigger }) {
       }
 
       playNextNote();
-      intervalRef.current = setInterval(playNextNote, 360);
+      intervalRef.current = setInterval(playNextNote, 450);
       setIsPlaying(true);
     } catch (e) {
       console.warn('Web Audio error:', e);
